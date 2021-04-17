9a88f91174c1cc78e79099ee701dd56aa3d40d29
Sagnik Mandal <sonalisagnik123@gmail.com>
2021-04-17 17:45:26 +0530

    Fix: CORS

--- main.py
+++ main.py
@@ -22,6 +22,14 @@ import models, resources
 def create_tables():
     db.create_all()
 
+@app.after_request
+def after_request(response):
+    response.headers["Access-Control-Allow-Origin"] = "*" # <- You can change "*" for a domain for example "http://localhost"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+    return response
+
 @app.route('/')
 def index():
   return jsonify({'message':'Hello World!'})
